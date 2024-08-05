9451894adc2a988eeabd2ee445059a835e528288
marco-soria <marco.soriaa7@gmail.com>
2024-08-04 21:06:04 -0500

    added scalar types

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -8,12 +8,25 @@ const { expressMiddleware } = require('@apollo/server/express4')
 const typeDefs = `
   type Query {
     hello: String
+    getPerson(name: String, age: Int): String
+    getInt(age: Int): Int
+    getFloat(price: Float): Float
+    getString: String
+    getBoolean: Boolean
+    getID: ID
   }
 `
 
 const resolvers = {
   Query: {
-    hello: () => 'Hola mundillo'
+    hello: () => 'Hello world',
+    getPerson: (_, args) => `Hello, my name is ${args.name}, I'm ${args.age} years old!`,
+    getInt: (_, args) => args.age,
+    getFloat: (_, args) => args.price,
+    getString: () => 'palabra',
+    getBoolean: () => true,
+    getID: () => '121212'
+
   }
 }
 
