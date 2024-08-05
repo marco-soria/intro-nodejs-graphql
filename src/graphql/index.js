d1c68fa2903d448688a0635d4f498f4a8b08b552
marco-soria <marco.soriaa7@gmail.com>
2024-08-05 16:46:02 -0500

    added custom scalars

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -3,13 +3,23 @@ const { expressMiddleware } = require('@apollo/server/express4');
 const { ApolloServerPluginLandingPageLocalDefault } = require('@apollo/server/plugin/landingPage/default')
 const { loadFiles } = require('@graphql-tools/load-files')
 const { buildContext } = require('graphql-passport')
+const { typeDefs: scalarsTypeDefs, resolvers: scalarsResolvers } = require('graphql-scalars');
+
 const resolvers = require('./resolvers')
 
 //crear servidor de GraphQL
 const useGraphql = async (app) => {
+    const typeDefs = [
+      ...await loadFiles('./src/**/*.graphql'),
+      scalarsTypeDefs,
+    ];
+    const allResolvers = [
+      resolvers,
+      scalarsResolvers,
+    ];
     const server = new ApolloServer({
-        typeDefs: await loadFiles('./src/**/*.graphql'), //llamada de schema.graphql
-        resolvers, // llamada de Querys y Mutations
+      typeDefs,
+      resolvers: allResolvers,// llamada de Querys y Mutations
         plugins: [
             // Install a landing page plugin based on NODE_ENV
             process.env.NODE_ENV === 'production'
