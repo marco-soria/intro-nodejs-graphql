1df7458370dde94c03b4f612145e7e0c44d9156c
marco-soria <marco.soriaa7@gmail.com>
2024-08-05 10:11:06 -0500

    added mutations

--- src/graphql/product.resolvers.js
+++ src/graphql/product.resolvers.js
@@ -12,4 +12,9 @@ const getProducts = async () => {
   return products;
 }
 
-module.exports = { getProduct, getProducts };
+const addProduct = async (_, { dto }) => {
+  const newProduct = await service.create(dto);
+  return newProduct;
+}
+
+module.exports = { getProduct, getProducts, addProduct };
